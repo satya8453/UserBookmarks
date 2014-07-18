d34a29d09d2391c5ee76de5e77accf108269bdec
satya8453 <das.satyaprakash@gmail.com>
2014-07-18 12:11:22 +0530

    DB URL changed to modulusio

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@
  */
 module.exports = {
 
-	'url' : 'mongodb://localhost:27017/test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	//'url' : 'mongodb://localhost:27017/test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://satya:123456@novus.modulusmongo.net:27017/nywuZ8ap'
 
 };
